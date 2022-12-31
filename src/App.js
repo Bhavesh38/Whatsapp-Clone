338fc9b78b9d643e53a303b712c9c53582716783
Bhavesh <bhavesh953475@gmail.com>
2022-12-31 20:36:50 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app_body">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Chat />} />
+            <Route
+              path="/rooms/:roomId"
+              element={
+                // <>
+                <Chat />
+                // </>
+              }
+            />
+          </Routes>
+        </div>
+      )}
     </div>
   );
 }
